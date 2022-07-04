b7582a090d2600851f4f104060e86c59b498994e
Matt Savoia <matt@1-800-rad-dude.com>
2022-07-04 15:45:29 -0400

    Update vimrc, init.lua, migrate plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,66 @@ vim.o.sw = 4
 -- Disable swapfiles
 vim.bo.swapfile = false
 
+-- highlight embedded lua
+vim.g.vimsyn_embed = 'l'
+
+
+-- Plugins
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+-- plug#begin has the effect of
+-- filetype plugin indent on
+
+Plug('morhetz/gruvbox')
+
+-- Deletes trailing whitespace on buffer write
+Plug('ntpeters/vim-better-whitespace')
+
+vim.g.better_whitespace_enabled = 0
+vim.g.strip_whitespace_on_save = 1
+vim.g.strip_whitespace_confirm = 0
+
+-- Fuzzy file finder
+Plug('junegunn/fzf')
+Plug('junegunn/fzf.vim')
+
+-- Allows changing the surrounding chars of a string with cs
+Plug('tpope/vim-surround')
+
+-- Allows for easy commenting out of lines and motions
+Plug('tpope/vim-commentary')
+
+-- Allows . command to repeat plugin actions
+Plug('tpope/vim-repeat')
+
+-- Git integration
+Plug('tpope/vim-fugitive')
+
+-- treesitter configs and abstraction layer
+Plug('nvim-treesitter/nvim-treesitter')
+-- Needed for telescope
+Plug('nvim-lua/plenary.nvim')
+-- Fuzzy finder
+Plug('nvim-telescope/telescope.nvim')
+-- open telescope file finder on cmd-P
+vim.keymap.set('n', '<C-p>', '<cmd>Telescope git_files<cr>', { noremap = true })
+
+-- Grep within working dir or repo
+Plug('mhinz/vim-grepper')
+vim.g.grepper = {
+    tools = {'rg', 'git'}
+}
+
+-- Use gs to take any motion and populate the search prompt
+vim.keymap.set({'n', 'x'}, 'gs', '<plug>(GrepperOperator)')
+-- Shortcut to open :Grepper
+vim.keymap.set('n', '<Leader>gg', ':Grepper<CR>', { silent = true })
+
+vim.call('plug#end')
+
+vim.cmd('colorscheme gruvbox')
+
 -- Shortcut to open this file
 vim.keymap.set('n', '<Leader>v', ':e ~/.config/nvim/init.lua<CR>', { silent = false })
 
@@ -63,4 +123,5 @@ vim.keymap.set('n', '<Leader>>', '/>>>>>>><CR>', { silent = true })
 vim.keymap.set('n', '<Leader>=', '/=======<CR>', { silent = true })
 
 -- Remove neovim mapping of Y to y$
-vim.keymap.del('n', 'Y')
+-- vim.keymap.del('n', 'Y')
+
