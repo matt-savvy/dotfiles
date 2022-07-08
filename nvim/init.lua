e3b2c97326a93f4b10b7eb7718811d651a7bef7e
Matt Savoia <matt@1-800-rad-dude.com>
2022-07-08 18:37:24 -0400

    Update rename mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -220,7 +220,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<Leader>R', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
