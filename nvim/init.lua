ab012448769d174cdf7eb72e59d1735fe2b158f4
Matt Savoia <matt@1-800-rad-dude.com>
2022-07-26 19:35:28 -0400

    Update null-ls setup, prettier on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -228,9 +228,9 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
     -- always show a sign column of width 1
     vim.wo.signcolumn = "yes:1"
+
     -- format on save if available
     -- if client.resolved_capabilities.document_formatting then
     --     vim.cmd [[augroup Format]]
@@ -267,26 +267,24 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
--- local null_ls = require('null-ls')
--- local eslint = require('eslint')
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require('null-ls')
 
--- null_ls.setup()
-
--- eslint.setup({
---   bin = 'eslint_d',
---   code_actions = {
---     enable = false,
---     apply_on_save = {
---       enable = false,
---     },
---     disable_rule_comment = {
---       enable = true,
---       location = "separate_line",
---     },
---   },
---   diagnostics = {
---     enable = true,
---     report_unused_disable_directives = false,
---     run_on = "type",
---   },
--- })
+null_ls.setup({
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.formatting({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
+    sources = {
+        null_ls.builtins.formatting.prettierd
+    },
+})
