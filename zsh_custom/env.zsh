e6455bd90b68080ccb09eaae37b08fcc906e0053
Matt Savoia <matt@1-800-rad-dude.com>
2023-01-29 13:08:33 -0500

    Remove NVM

--- zsh_custom/env.zsh
+++ zsh_custom/env.zsh
@@ -1,8 +1,3 @@
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 export npm_config_fund=false
 
 export HISTTIMEFORMAT="%d/%m/%y %T "
