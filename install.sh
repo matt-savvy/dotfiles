94364460a5a1b4988f77a973f523684d19152576
Matt Savoia <matt@1-800-rad-dude.com>
2023-02-14 20:36:50 -0500

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+nix-channel --update
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.vimPlugins.vim-plug
+nix-env -iA nixpkgs.tldr
+nix-env -iA nixpkgs.ripgrep
+nix-env -iA nixpkgs.gh
+nix-env -iA nixpkgs.fd
+nix-env -iA nixpkgs.fzf
+nix-env -iA nixpkgs.elixir_1_14
+nix-env -iA nixpkgs.nodejs
+nix-env -iA nixpkgs.flyctl
+
