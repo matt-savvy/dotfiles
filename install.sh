0dc19e6c9b26b25e83f160e3c4e15c34627493b3
Matt Savoia <matt@1-800-rad-dude.com>
2023-02-15 23:19:12 -0500

    Update install.sh, add shebang

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
 nix-channel --update
-nix-env -iA nixpkgs.neovim
-nix-env -iA nixpkgs.vimPlugins.vim-plug
-nix-env -iA nixpkgs.tldr
-nix-env -iA nixpkgs.ripgrep
-nix-env -iA nixpkgs.gh
-nix-env -iA nixpkgs.fd
-nix-env -iA nixpkgs.fzf
-nix-env -iA nixpkgs.elixir_1_14
-nix-env -iA nixpkgs.nodejs
-nix-env -iA nixpkgs.flyctl
-
+nix-env -iA nixpkgs.neovim \
+nixpkgs.vimPlugins.vim-plug \
+nixpkgs.tldr \
+nixpkgs.ripgrep \
+nixpkgs.gh \
+nixpkgs.fd \
+nixpkgs.fzf \
+nixpkgs.elixir_1_14 \
+nixpkgs.nodejs \
+nixpkgs.flyctl
