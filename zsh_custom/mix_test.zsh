2495fbacc9aee857e2d3bed0c6252b5abc5bd60b
Matt Savoia <matt@1-800-rad-dude.com>
2023-05-17 16:00:10 -0400

    Extract mix_test to mix_test.zsh

---
+++ zsh_custom/mix_test.zsh
@@ -0,0 +1,47 @@
+#!/bin/zsh
+
+function mix_test() {
+  QUERY=$1
+  TEST_DIR=$2
+
+  # no query
+  if [[ -z $QUERY ]]; then
+    mix test
+    return 0
+  fi
+
+  # query begins with test/
+  if [[ ${QUERY:0:5} == "test/" ]]; then
+      mix test $QUERY
+      return 0
+  fi
+
+  if [[ -z $TEST_DIR ]]; then
+      TEST_DIR=(`fd test -t d`)
+  fi;
+
+  # does this include a line number?
+  if test "${QUERY#*:}" != $QUERY; then
+      LINE_NUMBER=${QUERY#*:}
+      QUERY=${QUERY%%:*}
+      FILE=`fd -p $QUERY $TEST_DIR | head -n 1`
+      if [[ -z $FILE ]]; then
+          echo "No tests found."
+          return 1
+      fi
+      FILES=$FILE:$LINE_NUMBER
+  else
+      FILES=`fd -p $QUERY $TEST_DIR | head`
+  fi
+
+  # if $1 starts with test, use it as is, don't call fd
+
+  if [[ -z $FILES ]]; then
+      echo "No tests found."
+      return 1
+  else
+      echo "tests found: \n$FILES\n"
+      echo $FILES | xargs mix test
+      return 0
+  fi
+}
