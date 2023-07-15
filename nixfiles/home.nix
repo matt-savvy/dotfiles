be324b02a79b2a84895b28e22390b2f0ea8d7dbd
Matt Savoia <mattsavoia@thinkpad-x13.myfiosgateway.com>
2023-07-15 14:29:49 -0400

    Update home.packages

--- nixfiles/home.nix
+++ nixfiles/home.nix
@@ -17,16 +17,16 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  home.packages = with pkgs; [
+    colordiff
+    fd
+    flyctl
+    fzf
+    gh
+    ghc
+    ripgrep
+    tldr
+    tree
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
