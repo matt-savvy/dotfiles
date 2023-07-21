6a587b850116e0afe5e42bdc2b8426bf705b0147
Matt Savoia <matt@1-800-rad-dude.com>
2023-07-21 11:42:04 -0400

    Update env.zsh, add RPS1
    Remove right hand side prompt

--- zsh_custom/env.zsh
+++ zsh_custom/env.zsh
@@ -11,3 +11,11 @@ export GH_EDITOR="nvim"
 # elixir
 export ERL_AFLAGS="-kernel shell_history enabled"
 export KERL_BUILD_DOCS="yes"
+
+# Nix
+# From https://github.com/NixOS/nix/issues/3616
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+
+export RPS1=''
