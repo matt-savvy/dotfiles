81f0a434fd11d89095a563135fbde3af4bfdbb65
Matt Savoia <matt@1-800-rad-dude.com>
2023-07-21 10:55:11 -0400

    Update home.nix for thescore machine

--- nixfiles/home.nix
+++ nixfiles/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "mattsavoia";
-  home.homeDirectory = "/home/mattsavoia";
+  home.username = "matt.savoia";
+  home.homeDirectory = "/Users/matt.savoia";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
