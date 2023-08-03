4b250d8e8ec4dfdf28c809781d6af36820cd7534
Matt Savoia <matt@1-800-rad-dude.com>
2023-08-03 15:52:42 -0400

    Update home-manager, fix google-cloud-sdk
    Needs gke-gloud-auth-plugin

--- nixfiles/home.nix
+++ nixfiles/home.nix
@@ -27,7 +27,7 @@
     k9s
     kubectl
     kubectx
-    google-cloud-sdk
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
   ]; in [
     colordiff
     xclip
