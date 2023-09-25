25f6d808f06e284368374a0598729b9d027c280c
Matt Savoia <matt@1-800-rad-dude.com>
2023-09-25 09:34:22 -0400

    Update home.nix, extract theScore packages

--- nixfiles/home.nix
+++ nixfiles/home.nix
@@ -19,7 +19,13 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs; [
+  home.packages = with pkgs; let scorePackages = [
+    pinentry_mac
+    k9s
+    kubectl
+    kubectx
+    google-cloud-sdk
+  ]; in [
     colordiff
     xclip
     fd
@@ -45,14 +51,7 @@
     docker-compose
     postgresql
     colima
-    pinentry_mac
-    k9s
-    kubectl
-    kubectx
-    google-cloud-sdk
-  ];
-
-
+  ] ++ scorePackages;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
