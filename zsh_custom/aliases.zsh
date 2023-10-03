62e45d100277b698d33ec762c3f5cc03a2976d47
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-03 14:36:32 -0400

    Update aliases, add gccl

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -2,6 +2,7 @@
 alias glm="git log --oneline master.."
 alias gld="git log --oneline develop.."
 alias gccp="git rev-parse --short HEAD | tr -d '\n' | pbcopy"
+alias gccl="gh_commit | tr -d '\n' | pbcopy"
 alias gbc=git_branch
 alias gbb=brancher
 alias gbt=tagger
