9e282251c8dad69bfa040c81d99a30aaf566c7cb
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-04 16:29:06 -0400

    Update gh_file, add fallback for detached commit

--- zsh_custom/gh_file.zsh
+++ zsh_custom/gh_file.zsh
@@ -4,6 +4,11 @@ function gh_file() {
    REMOTE_URL=`git remote get-url origin`
    TRIMMED_URL=${REMOTE_URL:0:-4}
    COMMIT_ISH=`git rev-parse --abbrev-ref HEAD`
+
+   # do we have a detached head checked out?
+   if [[ $COMMIT_ISH == HEAD ]]; then
+       COMMIT_ISH=`git rev-parse --short HEAD`
+   fi;
    FILEPATH=$1
 
    echo "$TRIMMED_URL/blob/$COMMIT_ISH/$FILEPATH"
