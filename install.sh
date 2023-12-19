0b8a7e51dae4787d9d5b19e054fb425b7547fc41
Matt Savoia <matt@1-800-rad-dude.com>
2023-12-19 15:50:09 -0500

    Add install.sh for gitpod

--- install.sh
+++ install.sh
@@ -8,5 +8,7 @@ nix-env -iA nixpkgs.fd nixpkgs.ripgrep nixpkgs.fzf nixpkgs.tree
 
 # symlink config files
 mkdir /home/gitpod/.config/nvim
+rm /home/gitpod/.config/nvim/init.lua
+
 ln -s /home/gitpod/nvim/minimal.lua /home/gitpod/.config/nvim/init.lua
 alias vim=nvim
