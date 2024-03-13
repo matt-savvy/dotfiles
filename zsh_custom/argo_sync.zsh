88f3a82652548b2ec68cb58759f1466de3cb4dff
Matt Savoia <matt@1-800-rad-dude.com>
2024-03-13 15:37:26 -0400

    Add argo_list

--- zsh_custom/argo_sync.zsh
+++ zsh_custom/argo_sync.zsh
@@ -10,5 +10,19 @@ function argo_sync() {
     echo "SERVICE: $SERVICE"
     echo "ENV: $ENV"
 
-    argocd app list -p scorebet-$SERVICE -o name | grep "$SERVICE-helm-.*-$ENV" | xargs argocd app sync --async
+    # argocd app list -p scorebet-$SERVICE -o name --grpc-web | grep "$SERVICE-helm-.*-$ENV" | xargs argocd app sync --grpc-web
+    argocd app list -p scorebet-$SERVICE -o name --grpc-web | grep "$SERVICE-helm-.*-$ENV" | xargs argocd app sync --async --grpc-web
+}
+
+function argo_list() {
+    SERVICE=$1
+    ENV=$2
+    if [[ -z $ENV ]]; then
+        ENV=staging
+    fi
+
+    echo "SERVICE: $SERVICE"
+    echo "ENV: $ENV"
+
+    argocd app list -p scorebet-$SERVICE --grpc-web | grep "$SERVICE-helm-.*-$ENV"
 }
