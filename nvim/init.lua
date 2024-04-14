a0611858d45c63d39568d728730966ac29fa1786
Matt Savoia <matt@1-800-rad-dude.com>
2024-04-14 16:28:56 -0400

    Update init.lua, disable treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -268,16 +268,16 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
-require'nvim-treesitter.configs'.setup {
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
-  highlight = {
-    enable = true,
-  },
-}
+-- require'nvim-treesitter.configs'.setup {
+--   -- Install parsers synchronously (only applied to `ensure_installed`)
+--   sync_install = false,
+--   -- Automatically install missing parsers when entering buffer
+--   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+--   auto_install = false,
+--   highlight = {
+--     enable = true,
+--   },
+-- }
 
 
 local ls = require('luasnip')
