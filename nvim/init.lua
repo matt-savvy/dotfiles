51536d1e190d8550b6bdcba9a77b1861d65e6b1e
Matt Savoia <matt@1-800-rad-dude.com>
2024-08-29 20:17:48 -0400

    Update on_attach opts, fix missing bufopts

--- nvim/init.lua
+++ nvim/init.lua
@@ -218,16 +218,17 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '[w', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']w', vim.diagnostic.goto_next, opts)
     vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
     -- always show a sign column of width 1
     vim.wo.signcolumn = "yes:1"
 
