79cc1c21d07f51bd51bbbe1f59974b925688279e
Matt Savoia <matt@1-800-rad-dude.com>
2024-11-20 18:22:08 -0500

    Update personal.nix, extract username/homeDirectory

--- nixfiles/personal.nix
+++ nixfiles/personal.nix
@@ -2,4 +2,9 @@
 
 {
   imports = [ ./home.nix ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "mattsavoia";
+  home.homeDirectory = "/home/mattsavoia";
 }
