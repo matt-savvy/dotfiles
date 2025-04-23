60547a45cda0fa4ba213c0361dfd19b4e249a028
Matt Savoia <matt@1-800-rad-dude.com>
2025-04-23 14:28:43 -0400

    Rename base home.nix -> default.nix

--- nixfiles/hosts/thinkpad/home.nix
+++ nixfiles/hosts/thinkpad/home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ../../home.nix ];
+  imports = [ ../../default.nix ];
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
