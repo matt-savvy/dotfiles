fcecdcc68420f7196cdff8ff9fa43264b1865d60
Matt Savoia <matt@1-800-rad-dude.com>
2025-04-23 23:08:23 -0400

    Add hosts/macbook_pro_thescore

---
+++ nixfiles/hosts/macbook_pro_thescore/home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../../default.nix ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "matt.savoia";
+  home.homeDirectory = "/Users/matt.savoia";
+
+  programs.git = {
+    includes = [
+      { path = ../git/thescoreconfig; }
+    ];
+  };
+
+  home.packages = with pkgs; [
+    argocd
+    jq
+    openssl
+    protobuf
+    prototool
+    kubernetes-helm
+    pinentry_mac
+    k9s
+    kubectl
+    kubectx
+    vault
+    buf
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    mise = { enable = true; };
+  };
+
+  programs.mise = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+}
