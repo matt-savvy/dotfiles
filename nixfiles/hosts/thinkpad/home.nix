912c063b0b89192b6e889d7c7a30bc53719eb85a
Matt Savoia <matt@1-800-rad-dude.com>
2025-04-27 11:25:16 -0400

    Update thinkpad home, enable hardMode

--- nixfiles/hosts/thinkpad/home.nix
+++ nixfiles/hosts/thinkpad/home.nix
@@ -20,6 +20,8 @@
     vlc
   ];
 
+  hardMode = true;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
