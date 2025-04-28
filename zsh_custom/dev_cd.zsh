8ebf878e3ff83ab5c030f384017c748cdf439757
Matt Savoia <matt@1-800-rad-dude.com>
2025-04-28 10:16:46 -0400

    Refactor dev_cd script

--- zsh_custom/dev_cd.zsh
+++ zsh_custom/dev_cd.zsh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 function dev_cd {
-    DIR=$(find ~/dev -maxdepth 1 -type d -printf "%P\n" | fzf)
+    DIR=$(cd ~/dev && ls -d * | fzf)
 
-    if [[ -n ~/dev/$DIR ]]; then
+    if [[ $? -eq 0 && -n ~/dev/$DIR ]]; then
         cd ~/dev/$DIR
     fi
 }
