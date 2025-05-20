43f5e941bdca8765fa9ee13f3e890ae00039e1d3
Matt Savoia <matt@1-800-rad-dude.com>
2025-05-20 18:33:21 -0400

    Update configuration.nix for printing

--- nixfiles/hosts/thinkpad/configuration.nix
+++ nixfiles/hosts/thinkpad/configuration.nix
@@ -64,7 +64,18 @@
   };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = [
+      pkgs.cups-brother-hll2350dw
+    ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
