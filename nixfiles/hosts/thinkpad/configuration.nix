9710a4a4f8d2c0cfde5d55403d9ba021e63d6341
Matt Savoia <matt@1-800-rad-dude.com>
2025-08-19 20:04:08 -0400

    Update configuration, update firewall.allowedTCPPorts

--- nixfiles/hosts/thinkpad/configuration.nix
+++ nixfiles/hosts/thinkpad/configuration.nix
@@ -164,7 +164,8 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+
+  networking.firewall.allowedTCPPorts = [ 4000 443 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
